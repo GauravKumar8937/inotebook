9056b49180928c9fe336faef540e851fd5951899
Gaurav_kumar8 <92157748+GauravKumar8937@users.noreply.github.com>
2024-07-15 18:51:22 +0530

    font add

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -22,7 +22,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "66863b9650f5fb8473b4fa20",
+          "_id": "6686a3b9650f5fb8473b4fa20",
           "user": "6682e319f039d1704df00aa7",
           "title": "DSA  is must",
           "description": "DSA is compulsory for cracking good company",
@@ -31,7 +31,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-          "_id": "668c1195207de9f89c231de6",
+          "_id": "668c1195207de9af89c231de6",
           "user": "6682e319f039d1704df00aa7",
           "title": "MeritShot Drive",
           "description": "Today is my MeritShot drive",
@@ -40,7 +40,7 @@ const NoteState = (props)=>{
           "__v": 0
         },
         {
-            "_id": "6683df055cb34a43dfd3e6de",
+            "_id": "6683df055cb3a4a43dfd3e6de",
             "user": "6682e319f039d1704df00aa7",
             "title": "kuch bhi likh de",
             "description": "kuch bhi ke bare me likh de, aur kuch aur bhi",
@@ -49,7 +49,7 @@ const NoteState = (props)=>{
             "__v": 0
           },
           {
-            "_id": "6683df535cb34a43dfd3e6e0",
+            "_id": "6683df535cb34a4a3dfd3e6e0",
             "user": "6682e319f039d1704df00aa7",
             "title": "kuch bhi likh de",
             "description": "kuch bhi ke bare me likh de, aur kuch aur bhi",
@@ -58,7 +58,7 @@ const NoteState = (props)=>{
             "__v": 0
           },
           {
-            "_id": "66863b9650f5fb8473b4fa20",
+            "_id": "66a863b9650f5fb8473b4fa20",
             "user": "6682e319f039d1704df00aa7",
             "title": "DSA  is must",
             "description": "DSA is compulsory for cracking good company",
@@ -67,7 +67,7 @@ const NoteState = (props)=>{
             "__v": 0
           },
           {
-            "_id": "668c1195207de9f89c231de6",
+            "_id": "668c11952a07de9f89c231de6",
             "user": "6682e319f039d1704df00aa7",
             "title": "MeritShot Drive",
             "description": "Today is my MeritShot drive",
